b17fd17610348d4b03ed32892169521c902ffb1f
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 04:44:40 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,5 @@
-magic_exit_number = 1
 count = 10
-while count > 10 && count != magic_exit_number do
-  puts "I am the #{count}, Happy New Year!" # Work
-  count = count - 1
+while count >= 1 do
+ 
+  count -= 1
 end
